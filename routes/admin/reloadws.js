9ab466c5174bfd7e4757aa95c7b05481e0f42403
SirHyperNova <sirhypernova@gmail.com>
2018-05-23 22:48:20 +0000

    Fixed refresh

--- routes/admin/reloadws.js
+++ routes/admin/reloadws.js
@@ -3,10 +3,10 @@ module.exports = function (app, config, db, proxy, c9) {
     const path = require('path');
     return {
         all: (req,res) => {
-            c9._workspaces = fs.readdirSync(this._wsd).filter(name => {
+            c9._workspaces = fs.readdirSync(c9._wsd).filter(name => {
                 return !name.startsWith('.')
             }).filter(name => {
-                return fs.statSync(path.resolve(this._wsd,name)).isDirectory()
+                return fs.statSync(path.resolve(c9._wsd,name)).isDirectory()
             });
             res.redirect('back');
         }
